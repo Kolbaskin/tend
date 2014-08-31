6746797b4ff0d3342b760082bab0b6e84dadbedd
Kolbaskin <maximtushev@gmail.com>
2014-08-31 13:51:35 +0400

    new file:   protected/modules/tender/view/DataModel.inc 	modified:   protected/modules/tender/view/TenderOneEasyForm.tpl 	new file:   static/admin/locale/en.js 	new file:   static/admin/locale/ru.js 	modified:   static/style/inner.css

---
+++ static/admin/locale/ru.js
@@ -0,0 +1,18 @@
+TEXTS = {
+    "Gvsu-modules-distinations-model-DistinationsComboModel":"Направления (селектор)",
+    "Gvsu-modules-distinations-model-DistinationsModel":"Направления работ",
+    "Gvsu-modules-distinations-model-SelWorksModel":"Организации: выбранные направления",
+    "Gvsu-modules-distinations-model-WorksModel":"Направления работ: виды работ",
+    "Gvsu-modules-docs-model-DocTypesModel":"Справочник типов документов",
+    "Gvsu-modules-docs-model-OrgDocsModel":"Организации: документы",
+    "Gvsu-modules-orgs-model-OrgsModel":"Организации",
+    "Gvsu-modules-orgs-model-OrgsViewModel":"Заявки: информация об организации",
+    "Gvsu-modules-refs-model-CustomersModel":"Справочник контрагентов",
+    "Gvsu-modules-refs-model-ObjectsModel":"Справочник объектов",
+    "Gvsu-modules-tender-model-BidModel":"Заявки",
+    "Gvsu-modules-tender-model-BidPosModel":"Заявки: позиции",
+    "Gvsu-modules-tender-model-PositionsModel":"Тендер: позиции",
+    "Gvsu-modules-tender-model-SubjectsModel":"Тендер: предмет",
+    "Gvsu-modules-tender-model-TenderModel":"Тендер",
+    "Gvsu-modules-users-model-PublUsersModel":"Пользователи"
+}
