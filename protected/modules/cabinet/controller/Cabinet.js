6dcf79e133a619d86044dad5dbfadacd1ccd5f50
Kolbaskin <maximtushev@gmail.com>
2014-09-23 14:57:48 +0400

    modified:   protected/modules/cabinet/controller/Cabinet.js 	modified:   protected/modules/orgs/view/orgdata.inc 	new file:   protected/modules/tender/view/TenderOneEasyForm_new.tpl 	modified:   protected/modules/users/model/User.js 	modified:   protected/modules/users/view/registration.tpl 	modified:   static/js/registration.js

--- protected/modules/cabinet/controller/Cabinet.js
+++ protected/modules/cabinet/controller/Cabinet.js
@@ -1,6 +1,10 @@
 Ext.define('Gvsu.modules.cabinet.controller.Cabinet',{
     extend: "Core.Controller"
     
+    ,publicMethods: {
+        dashboard: 'User dashboard'
+    }
+    
     ,dashboard: function(params, cb) {
         var me = this
             ,data = {status: false, user: {}, doc_days: ''};
@@ -28,7 +32,6 @@ Ext.define('Gvsu.modules.cabinet.controller.Cabinet',{
                 me.tplApply('.cabinet', data, cb)        
             }
         ].runEach()
-        
     }
     
     
